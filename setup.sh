0bcb0b480665aedb25edfde82f561ef9a51295b6
lucas-mezencio <lucasmezss@hotmail.com>
2021-11-08 19:53:04 -0300

    add ide in snap

--- setup.sh
+++ setup.sh
@@ -48,8 +48,9 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 echo "Instalando openjdk 11"
 sudo pacman -S openjdk11-doc jdk11-openjdk
 
-
-
-
-
+echo "instalando ides via snap"
+sudo ln -s /var/lib/snapd/snap /snap
+sudo snap install intellij-idea-ultimate --classic
+sudo snap install android-studio --classic
+sudo snap install dbeaver-ce
 
