6c84f759b84f2902ec1636f5de13758fb4dca3fb
Lucas Mezencio <lucasmezss@hotmail.com>
2021-12-09 17:02:22 -0300

    add vim plug install

--- scripts/config-vim.sh
+++ scripts/config-vim.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
-
 pwd=$(pwd)
-cp $pwd/config/vim/.vimrc ~/.vimrc
-cp $pwd/config/vim/init.vim ~/.config/nvim/init.vim
+parent_folder=$(dirname $pwd)
 
+mkdir -p $HOME/.config/nvim
+cp -r $parent_folder/config/nvim $HOME/.config/
+
+# INSTALL VIM PLUG (PLUGIN MANAGER)
+
+echo $(sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim')
