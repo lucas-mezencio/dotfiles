4c7973c51cd8a8d2d3f608caf092783c48561b0a
lucas mezencio <lucasmezencio@outlook.com>
2024-03-17 14:00:43 -0300

    fix: base tmux config

--- setup.sh
+++ setup.sh
@@ -20,9 +20,31 @@ install_alacritty() {
     fi
 }
 
+install_tmux() {
+    echo "setting up tmux"
+    if ! command -v tmux &> /dev/null; then
+        echo "tmux not found"
+    else
+        # git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+        TPM_DIR=$HOME/.tmux/plugins/tpm
+        if [ ! -d "$TPM_DIR" ]; then
+            # echo "installing tpm first"
+            echo "install tpm first"
+            echo "git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm"
+            # git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+        else
+            echo "tpm already installed"
+        fi
+        echo "DONT FORGET TO INSTALL FZF VIA BREW # TODO: ZSH"
+        echo "installing tmux config"
+        ./scripts/install-tmux.sh $THISPATH
+    fi
+}
+
 if [ "$#" -eq 0 ]; then
     install_nvim
     install_alacritty
+    install_tmux
 elif [ "$#" -eq 1 ]; then
     case "$1" in 
         "nvim")
@@ -30,6 +52,9 @@ elif [ "$#" -eq 1 ]; then
             ;; 
         "alacritty")
         install_alacritty
+            ;;
+        "tmux")
+        install_tmux
             ;;
         *)
         echo "invalid option"
