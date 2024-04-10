660f5c7e36fd29b604f9e6da5184442a391bf513
lucas mezencio <lucasmezencio@outlook.com>
2024-04-10 18:32:31 -0300

    feat: install nvim via setup script

--- setup.sh
+++ setup.sh
@@ -2,38 +2,6 @@
 
 THISPATH=$(dirname "$(readlink -f "$0")")
 
-
-install_alacritty() {
-    if ! command -v alacritty &> /dev/null; then 
-        echo "alacritty not found"
-    else
-        echo "installing alacritty config"
-        ./scripts/install-alacritty.sh $THISPATH
-    fi
-}
-
-install_tmux() {
-    echo "setting up tmux"
-    if ! command -v tmux &> /dev/null; then
-        echo "tmux not found"
-    else
-        # git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-        TPM_DIR=$HOME/.tmux/plugins/tpm
-        if [ ! -d "$TPM_DIR" ]; then
-            # echo "installing tpm first"
-            echo "install tpm first"
-            echo "git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm"
-            # git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-        else
-            echo "tpm already installed"
-        fi
-        echo "DONT FORGET TO INSTALL FZF VIA BREW # TODO: ZSH"
-        echo "installing tmux config"
-        ./scripts/install-tmux.sh $THISPATH
-    fi
-}
-
-
 install_ansible () {
     if command -v ansible &> /dev/null; then
         echo "ansible already installed"
@@ -42,31 +10,36 @@ install_ansible () {
         sudo apt install -y ansible
     fi
 }
-install_ansible
 
-if [ "$#" -eq 0 ]; then
-    # install_nvim
-    install_alacritty
-    install_tmux
-elif [ "$#" -eq 1 ]; then
-    case "$1" in 
-        # "nvim")
-        # install_nvim
-        #     ;; 
-        "alacritty")
-        install_alacritty
-            ;;
-        "tmux")
-        install_tmux
-            ;;
-        *)
-        echo "invalid option"
-        exit 1
-            ;;
-    esac
-else
-    echo "invalid options, only one argument accepted"
-    exit 1
-fi
+main () {
+    install_ansible
+    ansible-playbook setup/dotfiles.yaml -K
+}
+main
 
+# if [ "$#" -eq 0 ]; then
+#     install_nvim
+#     install_alacritty
+#     install_tmux
+# elif [ "$#" -eq 1 ]; then
+#     case "$1" in 
+#         "nvim")
+#         install_nvim
+#             ;; 
+#         "alacritty")
+#         install_alacritty
+#             ;;
+#         "tmux")
+#         install_tmux
+#             ;;
+#         *)
+#         echo "invalid option"
+#         exit 1
+#             ;;
+#     esac
+# else
+#     echo "invalid options, only one argument accepted"
+#     exit 1
+# fi
+#
 
