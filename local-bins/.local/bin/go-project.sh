5e8388222a01502217d722ce2d9de9bb530fb08d
lucas <lucasmezss@hotmail.com>
2025-06-27 17:45:08 -0300

    add golang starter script

---
+++ local-bins/.local/bin/go-project.sh
@@ -0,0 +1,31 @@
+#!/bin/env bash
+
+PROJECT=$1
+PROJECT_DIR=~/Projects/"$project"
+
+mkdir -p "$PROJECT_DIR"
+
+cd "$PROJECT_DIR"
+
+mkdir -p cmd/foo/
+touch cmd/foo/main.go
+
+mkdir -p pkg/config/
+touch pkg/config/config.go
+
+mkdir -p pkg/logger/
+touch pkg/logger/logger.go
+
+mkdir -p internal/domain/
+touch internal/domain/domain_entity.go
+
+mkdir -p internal/usecase/
+touch internal/usecase/entity_uc.go
+
+mkdir -p internal/infra/database/
+touch internal/infra/database/db.go
+
+mkdir -p internal/infra/gin/
+touch internal/infra/gin/server.go
+
+go mod init "$PROJECT"
