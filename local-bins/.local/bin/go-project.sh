0322a8aa6dbfa3ff555775f96b76bd0ce41f6d25
lucas <lucasmezss@hotmail.com>
2025-06-27 17:59:45 -0300

    add package definition on files

--- local-bins/.local/bin/go-project.sh
+++ local-bins/.local/bin/go-project.sh
@@ -9,23 +9,30 @@ cd $PROJECT_DIR
 
 mkdir -p cmd/foo/
 touch cmd/foo/main.go
+echo "package main" >> cmd/foo/main.go
 
 mkdir -p pkg/config/
 touch pkg/config/config.go
+echo "package config" >> pkg/config/config.go
 
 mkdir -p pkg/logger/
 touch pkg/logger/logger.go
+echo "package logger" >> pkg/logger/logger.go
 
 mkdir -p internal/domain/
 touch internal/domain/domain_entity.go
+echo "package domain" >> internal/domain/domain_entity.go
 
 mkdir -p internal/usecase/
 touch internal/usecase/entity_uc.go
+echo "package usecase" >> internal/usecase/entity_uc.go
 
 mkdir -p internal/infra/database/
 touch internal/infra/database/db.go
+echo "package database" >> internal/infra/database/db.go
 
 mkdir -p internal/infra/gin/
 touch internal/infra/gin/server.go
+echo "package gin" >> internal/infra/gin/server.go
 
 go mod init "$PROJECT"
