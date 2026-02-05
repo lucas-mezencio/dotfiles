c25a4f2e7098a03751f17401c9a6ffc9c753f70f
Lucas Mezencio <lucasmezencio@outlook.com>
2026-02-05 20:27:29 -0300

    fix(nvim): let copilot be default extra

--- nvim/.config/nvim/lua/extras/extras.lua
+++ nvim/.config/nvim/lua/extras/extras.lua
@@ -1,10 +1,5 @@
 return {
-  {
-    import = "lazyvim.plugins.extras.ai.copilot",
-    opts = {
-      panel = { enable = true },
-    },
-  },
+  { import = "lazyvim.plugins.extras.ai.copilot" },
   { import = "lazyvim.plugins.extras.ai.copilot-chat" },
   { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
