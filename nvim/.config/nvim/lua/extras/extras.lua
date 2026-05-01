35649565e35287eb6f223aa2404645b0c9110fac
Lucas Mezencio <lucasmezencio@outlook.com>
2026-05-01 13:47:26 -0300

    chore(copilot): remove github copilot

--- nvim/.config/nvim/lua/extras/extras.lua
+++ nvim/.config/nvim/lua/extras/extras.lua
@@ -1,6 +1,4 @@
 return {
-  { import = "lazyvim.plugins.extras.ai.copilot" },
-  { import = "lazyvim.plugins.extras.ai.copilot-chat" },
   { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
   { import = "lazyvim.plugins.extras.lang.clangd" },
